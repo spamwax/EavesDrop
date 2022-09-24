d2cc87d1bd7ed47d2c8881c54408a10021d2a497
Hamid Ghadyani <hamid@hamid.cc>
2022-09-24 10:04:00 -0400

    Fake release to test wago.io hooks (try 9)

--- .github/workflows/upload2wago.sh
+++ .github/workflows/upload2wago.sh
@@ -27,6 +27,7 @@ echo -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\"
 # curl -f -X POST -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\" -H \"authorization: Bearer $WAGO_API_TOKEN\" -H \"accept: application/json\" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
 curl -f -X POST -F "metadata=$metadata" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_TOKEN" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
 
+
 # echo curl -f -X POST -F \"metadata='{' \"lable\": \"$TAG_NAME\", \"stability\": \"stable\",\
 #     \"changelog\": \"### Release v$TAG_NAME\",\
 #     \"supported_retail_patch\": \"9.2.7\", \"supported_retail_patch\": \"3.4.0\" '}'\"\
