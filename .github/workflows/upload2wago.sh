8595957483165b11de17e2cbb42a6af01c0ae03d
Hamid Ghadyani <hamid@hamid.cc>
2022-09-24 09:48:40 -0400

    Fake release to test wago.io upload (try 5)

--- .github/workflows/upload2wago.sh
+++ .github/workflows/upload2wago.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -x
 
-read -r -d '' metadata <<'END'
+# read -r -d '' metadata <<'END'
+cat <<END > metadata.txt
 {
   "lable": "$TAG_NAME",
   "stability": "stable",
@@ -10,10 +12,22 @@ read -r -d '' metadata <<'END'
 }
 END
 
+read -r metadata <<< metadata.txt
+metadata=$(<metadata.txt)
 echo $metadata
+rm metadata.txt
 echo
-ls -l
 echo
 
 # curl -f -X POST -F "metadata=$metadata" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_KEY" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
-echo "curl -f -X POST -F metadata=$metadata -F file=@EavesDrop.zip -H authorization: Bearer $WAGO_API_KEY -H accept: application/json https://addons.wago.io/api/projects/$WAGO_PROJECT_ID/version"
+echo -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\"
+
+
+curl -f -X POST -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\" -H \"authorization: Bearer $WAGO_API_KEY\" -H \"accept: application/json\" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
+
+# echo curl -f -X POST -F \"metadata='{' \"lable\": \"$TAG_NAME\", \"stability\": \"stable\",\
+#     \"changelog\": \"### Release v$TAG_NAME\",\
+#     \"supported_retail_patch\": \"9.2.7\", \"supported_retail_patch\": \"3.4.0\" '}'\"\
+#     -F \"file=@EavesDrop.zip\"\
+#     -H \"authorization: Bearer $WAGO_API_KEY\"\
+#     -H \"accept: application/json\" https://addons.wago.io/api/projects/$WAGO_PROJECT_ID/version
