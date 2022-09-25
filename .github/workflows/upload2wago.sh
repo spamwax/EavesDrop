5d40cdc463f2d6090042214303f7bc775d9299e5
Hamid Ghadyani <hamid@hamid.cc>
2022-09-25 10:01:13 -0400

    chore: Update CI scripts.

--- .github/workflows/upload2wago.sh
+++ .github/workflows/upload2wago.sh
@@ -2,29 +2,28 @@
 set -x
 
 # read -r -d '' metadata <<'END'
-cat <<END > metadata.txt
+cat <<END > metadata.json
 {
   "lable": "$TAG_NAME",
   "stability": "stable",
-  "changelog": "### Release v$TAG_NAME",
+  "changelog": "Release v$TAG_NAME",
   "supported_retail_patch": "9.2.7"
 }
 END
 
-read -r metadata <<< metadata.txt
-metadata=$(<metadata.txt)
+# read -r metadata <<< metadata.json
+metadata=$(<metadata.json)
+echo
 echo $metadata
-rm metadata.txt
 echo
 ls -l
 echo
-
 # curl -f -X POST -F "metadata=$metadata" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_KEY" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
 # echo -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\"
 
 
 # curl -f -X POST -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\" -H \"authorization: Bearer $WAGO_API_TOKEN\" -H \"accept: application/json\" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
-curl -f -X POST -F "metadata=$metadata" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_TOKEN" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
+curl -f -X POST -F "metadata=<metadata.json" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_TOKEN" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
 
 
 # echo curl -f -X POST -F \"metadata='{' \"lable\": \"$TAG_NAME\", \"stability\": \"stable\",\
