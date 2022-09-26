a9ec82cc5881ea13f9d1aac681a54190bbc63617
Hamid Ghadyani <hamid@hamid.cc>
2022-09-25 20:33:15 -0400

    Separate Wrath from Mainline
    - Fix wago integration issues

--- .github/workflows/upload2wago.sh
+++ .github/workflows/upload2wago.sh
@@ -1,34 +1,25 @@
 #!/usr/bin/env bash
 set -x
 
-# read -r -d '' metadata <<'END'
 cat <<END > metadata.json
 {
   "label": "$TAG_NAME",
   "stability": "stable",
-  "changelog": "Release v$TAG_NAME",
+  "changelog": "Release v$TAG_NAME
+  $CHANGELOG",
   "supported_retail_patch": "9.2.7"
 }
 END
 
-# read -r metadata <<< metadata.json
 metadata=$(<metadata.json)
 echo
-echo $metadata
+echo "$metadata"
 echo
 ls -l
 echo
-# curl -f -X POST -F "metadata=$metadata" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_KEY" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
-# echo -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\"
 
-
-# curl -f -X POST -F \"metadata=$metadata\" -F \"file=@EavesDrop.zip\" -H \"authorization: Bearer $WAGO_API_TOKEN\" -H \"accept: application/json\" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
-curl -f -X POST -F "metadata=<metadata.json" -F "file=@EavesDrop.zip" -H "authorization: Bearer $WAGO_API_TOKEN" -H "accept: application/json" https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version
-
-
-# echo curl -f -X POST -F \"metadata='{' \"lable\": \"$TAG_NAME\", \"stability\": \"stable\",\
-#     \"changelog\": \"### Release v$TAG_NAME\",\
-#     \"supported_retail_patch\": \"9.2.7\", \"supported_retail_patch\": \"3.4.0\" '}'\"\
-#     -F \"file=@EavesDrop.zip\"\
-#     -H \"authorization: Bearer $WAGO_API_KEY\"\
-#     -H \"accept: application/json\" https://addons.wago.io/api/projects/$WAGO_PROJECT_ID/version
+curl -f -X POST -F "metadata=<metadata.json"\
+  -F "file=@EavesDrop.zip"\
+  -H "authorization: Bearer $WAGO_API_TOKEN"\
+  -H "accept: application/json"\
+  https://addons.wago.io/api/projects/"$WAGO_PROJECT_ID"/version && rm -rf metadata.json
