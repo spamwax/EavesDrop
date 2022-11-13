0b220842ba16a799509b6afb39b05fdc43af7c29
Hamid <hamid@hamid.cc>
2022-11-13 00:23:40 -0500

    Small cleanup

--- options.lua
+++ options.lua
@@ -680,8 +680,7 @@ function EavesDrop:SetupOptions()
             set = function(info, v)
               local key = info.arg or info[#info]
               wipe(EavesDrop.db.profile[key])
-              local idx
-              idx = 0
+              local idx = 0
               local auras = { strsplit("\n,", strtrim(v)) }
               for _, name in pairs(auras) do
                 (function()
@@ -694,21 +693,12 @@ function EavesDrop:SetupOptions()
                   if aura_id then
                     aura_id = _G["tonumber"](strtrim(aura_id, "\r\n\t -"))
                   end
-                  if aura_name and aura_id then
-                    print()
-                    -- print(string.format("-> full data", aura_name, aura_id))
-                  elseif aura_name then
+                  if aura_name and not aura_id then
                     aura_id = _G["tonumber"](aura_name)
                     if aura_id then
                       aura_name = GetSpellInfo(aura_id)
-                      --[[if aura_name then
-                        print(string.format("*only id*"))
-                      end]]
                     else
                       aura_id = select(7, GetSpellInfo(aura_name))
-                      --[[if aura_id then
-                        print(string.format("*only spellname*"))
-                      end]]
                     end
                   end
                   if not aura_name or not aura_id or tonumber(aura_name) then return end
@@ -716,7 +706,6 @@ function EavesDrop:SetupOptions()
                     return
                   end
                   idx = idx + 1
-                  -- print(string.format("-> inserting {%s, %d}", aura_name, aura_id))
                   EavesDrop.db.profile[key][aura_id] = aura_name
                   -- print(string.format("idx: %d, size of db table: %d", idx, #EavesDrop.db.profile[key]))
                 end)()
